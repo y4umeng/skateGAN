a1cef7d2c7a693fde50285465e45a7879c77901a
Yau-Meng Wong <102400085+y4umeng@users.noreply.github.com>
2024-05-04 16:37:49 +0800

    pytorch3d installation

--- install_pytorch3D.py
+++ install_pytorch3D.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
         # !pip install fvcore iopath
         if sys.platform.startswith("linux"):
             print("Trying to install wheel for PyTorch3D")
-            os.system(f'pip install --no-index --no-cache-dir pytorch3d -f https://dl.fbaipublicfiles.com/pytorch3d/packaging/wheels/{version_str}/download.html')
+            print(f'pip install --no-index --no-cache-dir pytorch3d -f https://dl.fbaipublicfiles.com/pytorch3d/packaging/wheels/{version_str}/download.html')
             pip_list = 'pip freeze'
             need_pytorch3d = not any(i.startswith("pytorch3d==") for  i in pip_list)
         if need_pytorch3d:
