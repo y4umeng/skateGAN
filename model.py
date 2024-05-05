10a69e723dd8d383f3ae93cb8208f91254638b40
Yau-Meng Wong <102400085+y4umeng@users.noreply.github.com>
2024-05-05 17:17:04 +0800

    removing skateGAN. going MAE mode

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 import torch.nn as nn
 from mae import *
-from utils import pose_generator
+from utils import *
 
 class skateMAE(torch.nn.Module):
     def __init__(self, encoder : MAE_Encoder, num_classes_dist=100, num_classes_elev=360, num_classes_azim=360) -> None:
@@ -27,18 +27,18 @@ class skateMAE(torch.nn.Module):
         classes = torch.argmax(torch.nn.LogSoftmax(logits), dim=1)
         return classes * 2.0
 
-class skateGAN(torch.nn.Module):
-    def __init__(self, obj_path, batch_size, device, img_size=64, patch_size=4) -> None:
-        super().__init__()
-        self.encoder = MAE_Encoder(image_size=img_size, patch_size=patch_size)
-        self.decoder = MAE_Decoder()
-        self.camera_position_heads = [ViT_Classifier(self.encoder, 180)] * 3
-        self.pose_image_generator = pose_generator(obj_path, img_size, batch_size, device)
-    def forward(self, curr_imgs, prev_imgs):
-        encoded_curr = self.encoder(curr_imgs)
-        encoded_prev = self.encoder(prev_imgs)
-        cam_pos = [head(encoded_curr) for head in self.camera_position_heads]
-        generated_poses = pose_generator(cam_pos[0], cam_pos[1], cam_pos[2])
-        encoded_gen = self.encoder(generated_poses)
-        predicted_imgs = self.decoder(encoded_prev, encoded_gen)
-        return predicted_imgs, cam_pos
+# class skateGAN(torch.nn.Module):
+#     def __init__(self, obj_path, batch_size, device, img_size=64, patch_size=4) -> None:
+#         super().__init__()
+#         self.encoder = MAE_Encoder(image_size=img_size, patch_size=patch_size)
+#         self.decoder = MAE_Decoder()
+#         self.camera_position_heads = [ViT_Classifier(self.encoder, 180)] * 3
+#         self.pose_image_generator = pose_generator(obj_path, img_size, batch_size, device)
+#     def forward(self, curr_imgs, prev_imgs):
+#         encoded_curr = self.encoder(curr_imgs)
+#         encoded_prev = self.encoder(prev_imgs)
+#         cam_pos = [head(encoded_curr) for head in self.camera_position_heads]
+#         generated_poses = pose_generator(cam_pos[0], cam_pos[1], cam_pos[2])
+#         encoded_gen = self.encoder(generated_poses)
+#         predicted_imgs = self.decoder(encoded_prev, encoded_gen)
+#         return predicted_imgs, cam_pos
