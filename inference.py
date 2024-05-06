32eace606bfd579473b9240213eb85a50a871298
Yau-Meng Wong <102400085+y4umeng@users.noreply.github.com>
2024-05-06 19:02:01 +0800

    inference

--- inference.py
+++ inference.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
     
     print(f"Num frames: {len(frame_paths)}")
 
-    frames = torch.zeros((len(frame_paths), 3, 64, 64))
+    frames = torch.zeros((len(frame_paths), 3, 32, 32))
     for fp in frame_paths:
         frame = transform(Image.open(frame_paths[fp]))
         frames[fp,...] = frame
